d027278fa858cedffdeca26deab6826c6d959600
Ayush Sonar <ayushsonar@atharvacoe.ac.in>
2023-10-09 06:12:58 +0530

    updated models with wattage field and updated path of images

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = "SingularSystems Admin"
+admin.site.site_title = "SingularSystems Admin Portal"
+admin.site.index_title = "Welcome to SingularSystems Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
